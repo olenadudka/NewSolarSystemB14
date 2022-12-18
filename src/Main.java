9a15378d429318dfc392eb6a568d41e4197e0c28
olenadudka <helen.lady2311@gmail.com>
2022-12-18 12:48:48 -0500

    Feature has been added but has some issues that we need to resolve

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,10 @@ public class Main {
         XX.sun=1;
        XX.moon=2;
        XX.planet=3;
+
+        FeatureSun Xeron = new FeatureSun();
+        Xeron.Heat="12324234";
+        Xeron.size=1221334234;
+        Xeron.Temperature="3434";
     }
 }
